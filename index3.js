c3a407570134ef4d7f8ac7d429e2602455644a7f
MantizA <73174186+MantizA@users.noreply.github.com>
2020-10-20 20:58:47 -0400

    Update index3.js

--- index3.js
+++ index3.js
@@ -1,12 +1,14 @@
 const puppeteer = require ('puppeteer');
 (async() =>{
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 926 });
     await page.goto('https://www.pokemon.com/us/pokedex/');
-
+    const form = await page.$('#loadMore > span');
+    await form.evaluate( form => form.click() );
+    await autoScroll(page);
     await page.waitForSelector("h5");
-    await page.waitFor(3000);
+    await page.waitFor(15000);
     let datospokemon = await page.evaluate(() => {
         let nombrepokemon = [];
         let pokenombre = document.querySelectorAll('section.pokedex-results > ul > li');
@@ -30,3 +32,22 @@ const puppeteer = require ('puppeteer');
     console.log(datospokemon);
     await browser.close();
 })();
+
+async function autoScroll(page){
+  await page.evaluate(async () => {
+      await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+              var scrollHeight = document.body.scrollHeight;
+              window.scrollBy(0, distance);
+              totalHeight += distance;
+
+              if(totalHeight >= scrollHeight){
+                  clearInterval(timer);
+                  resolve();
+              }
+          }, 100);
+      });
+  });
+}
